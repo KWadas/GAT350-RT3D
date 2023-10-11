eac235e12fb7a2d92cdf275783e6dd57a99d16b3
KWadas <kwadas@student.neumont.edu>
2023-10-11 12:07:02 -0600

    Added texture application.

--- Source/Application/World03.h
+++ Source/Application/World03.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Framework/World.h"
-#include "Core/Math/Vector2.h"
 #include "Renderer/Renderer.h"
 #include <vector>
 
@@ -15,12 +14,13 @@ namespace nc
 		void Draw(Renderer& renderer) override;
 
 	private:
-		float m_angle = 0;
 		float m_time;
 		float m_speed = 5;
-		glm::vec3 m_position;
+
+		Transform m_transform;
 
 		GLuint m_vao = 0;
 		res_t<Program> m_program;
+		res_t<Texture> m_texture;
 	};
 }
